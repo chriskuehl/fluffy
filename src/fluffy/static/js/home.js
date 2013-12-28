dece3f9cad4b4440b5a481ff492de9b51b8d6c9d
Chris Kuehl <chris@techxonline.net>
2013-12-27 20:13:44 -0800

    Add drop zone basic event handling.

--- src/fluffy/static/js/home.js
+++ src/fluffy/static/js/home.js
@@ -1,9 +1,37 @@
 $(document).ready(function() {
+	// browse button
 	$("#selectFile").click(function() {
 		$("#selectFileInput").trigger("click");
 	});
 
 	$("#selectFileInput").change(function() {
 		console.log($(this).val());
+	});
+
+
+	// drop zone file drag-and-drop
+	var dropZone = $("#dropZone");
+
+	dropZone.on("dragenter", function(e) {
+		e.preventDefault();
+		e.stopPropagation();
+	});
+
+	dropZone.on("dragover", function(e) {
+		$(this).css("backgroundColor", "#FFEECA");
+
+		e.preventDefault();
+		e.stopPropagation();
+	});
+
+	dropZone.on("dragleave", function() {
+		$(this).css("backgroundColor", "#F3FFE2");
+	});
+
+	dropZone.on("drop", function(e) {
+		e.preventDefault();
+
+		alert("dropped.");
+		return false;
 	});
 });
