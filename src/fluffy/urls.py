8851c576a2a5767bccc0b2d8cbcc807fedd95a3d
Chris Kuehl <chris@techxonline.net>
2013-12-28 18:41:30 -0800

    Remove examples from urls.

--- src/fluffy/urls.py
+++ src/fluffy/urls.py
@@ -1,9 +1,6 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'fluffy.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 	url(r'^$', 'fluffy.views.index', name='index'),
 	url(r'^upload$', 'fluffy.views.upload', name='upload')
 )
