1c4cb5480dda7c024b31b522a41e25ec24e0f232
Chris Kuehl <chris@techxonline.net>
2013-12-28 18:35:21 -0800

    Add test view for upload.

--- src/fluffy/urls.py
+++ src/fluffy/urls.py
@@ -4,5 +4,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'fluffy.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-	url(r'^$', 'fluffy.views.index', name='index')
+	url(r'^$', 'fluffy.views.index', name='index'),
+	url(r'^upload$', 'fluffy.views.upload', name='upload')
 )
--- src/fluffy/views.py
+++ src/fluffy/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def index(request):
 	return render(request, "index.html")
+
+def upload(request):
+	return HttpResponse("test")
