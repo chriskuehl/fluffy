c1b5e08ffd7e80444875eda2aecb8df4984cea8b
Chris Kuehl <chris@techxonline.net>
2013-12-29 22:08:17 -0800

    Add get_human_size based on the same function in JS to utils.

--- src/fluffy/utils.py
+++ src/fluffy/utils.py
@@ -11,3 +11,23 @@ def get_backend():
 	name, options = conf["name"], conf["options"]
 
 	return backends[name](options)
+
+
+ONE_GB = 1073741824
+ONE_MB = 1048576
+ONE_KB = 1024
+
+def get_human_size(size):
+	"""Convert a byte count into a human-readable size string like "4.2 MB".
+
+	Roughly based on Apache Commons FileUtils#byteCountToDisplaySize:
+	https://commons.apache.org/proper/commons-io/ """
+
+	if size / ONE_GB >= 1:
+		return "{:.1f} GB".format(size / ONE_GB)
+	elif size / ONE_MB >= 1:
+		return "{:.1f} MB".format(size / ONE_MB)
+	elif size / ONE_KB >= 1:
+		return "{:.1f} KB".format(size / ONE_KB)
+	else:
+		return "{} bytes".format(size)
