62335747aab1c57aba893b4c79550118968f2ee8
Chris Kuehl <chris@techxonline.net>
2013-12-29 18:03:17 -0800

    Iterate through uploaded files and process as a StoredFile.

--- src/fluffy/views.py
+++ src/fluffy/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from fluffy.models import StoredFile
 
 def index(request):
 	return render(request, "index.html")
 
 def upload(request):
+	files = []
+
+	for file in request.FILES.getlist("file"):
+		sfile = StoredFile(file)
+		files.append(sfile)
+
 	return HttpResponse("test")
