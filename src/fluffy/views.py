34ed85291724b73a7fff9931da2d44e1e386ee3c
Chris Kuehl <chris@techxonline.net>
2013-12-29 18:38:24 -0800

    Track file storing time and print debug messages.

--- src/fluffy/views.py
+++ src/fluffy/views.py
@@ -1,3 +1,4 @@
+import time
 from django.shortcuts import render
 from django.http import HttpResponse
 from fluffy.models import StoredFile
@@ -6,10 +7,23 @@ def index(request):
 	return render(request, "index.html")
 
 def upload(request):
-	files = []
+	"""Process an upload, storing each uploaded file with the configured
+	storage backend. Redirects to a status page which displays the uploaded
+	file(s).
 
-	for file in request.FILES.getlist("file"):
-		sfile = StoredFile(file)
-		files.append(sfile)
+	Returns JSON containing the URL to redirect to; the actual redirect is done
+	by the JavaScript on the upload page.
+	"""
+	file_list = request.FILES.getlist("file")
+	stored_files = [StoredFile(file) for file in file_list]
+
+	start = time.time()
+	print("Storing {} files...".format(len(stored_files)))
+
+	for stored_file in stored_files:
+		print("Storing {}...".format(stored_file.name))
+
+	elapsed = time.time() - start
+	print("Stored {} files in {:.1f} seconds.".format(len(stored_files), elapsed))
 
 	return HttpResponse("test")
