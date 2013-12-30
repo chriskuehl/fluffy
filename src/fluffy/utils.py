5b451908ddbd38c3276f552f854d5a5f041b1744
Chris Kuehl <chris@techxonline.net>
2013-12-30 15:30:14 -0800

    Add methods for encoding and decoding objects into strings.

--- src/fluffy/utils.py
+++ src/fluffy/utils.py
@@ -1,3 +1,6 @@
+import json
+import zlib
+import base64
 from django.conf import settings
 from fluffy.backends import FileBackend, S3CommandLineBackend
 
@@ -32,3 +35,21 @@ def get_human_size(size):
 		return "{:.1f} KB".format(size / ONE_KB)
 	else:
 		return "{} bytes".format(size)
+
+def encode_obj(obj):
+	"""Encodes an object for inclusion in a query string.
+
+	Works internally by serializing the object into JSON, gzipping the JSON,
+	and then base64 encoding it.
+	"""
+	serialized = json.dumps(obj)
+	zipped = zlib.compress(serialized.encode("utf-8"))
+
+	return base64.urlsafe_b64encode(zipped).decode("utf-8")
+
+def decode_obj(encoded):
+	"""Decodes a string encoded by encode_obj into an object."""
+	zipped = base64.urlsafe_b64decode(encoded.encode("utf-8"))
+	serialized = zlib.decompress(zipped).decode("utf-8")
+
+	return json.loads(serialized)
