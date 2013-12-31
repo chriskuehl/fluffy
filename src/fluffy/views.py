eabd30dad03d8d55d4af270b39a7d2749e94f25d
Chris Kuehl <chris@techxonline.net>
2013-12-30 18:37:05 -0800

    Fetch full details from small detail tuple.

--- src/fluffy/views.py
+++ src/fluffy/views.py
@@ -4,6 +4,7 @@ import json
 from django.core.urlresolvers import reverse
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.conf import settings
 from fluffy.models import StoredFile
 from fluffy.utils import get_backend, get_human_size, encode_obj, decode_obj
 from fluffy.backends import BackendException
@@ -80,5 +81,22 @@ def details(request, enc=encode_obj([])):
 
 	enc is the encoded list of detail tuples, as returned by get_details.
 	"""
-	details = decode_obj(enc)
-	return HttpResponse(str(details))
+	req_details = decode_obj(enc)
+	details = [get_full_details(file) for file in req_details]
+
+	return render(request, "details.html", {"details": details})
+
+def get_full_details(file):
+	"""Returns a dictionary of details for a file given a detail tuple."""
+	stored_name = file[0]
+	name = file[1] # original file name
+	size = file[2]
+	ext = os.path.splitext(stored_name)[1]
+
+	return {
+		"download_url": settings.FILE_URL.format(name=stored_name),
+		"info_url": settings.INFO_URL.format(name=stored_name),
+		"name": name + ext, # original name is stored w/o extension
+		"size": size,
+		"extension": ext[1:] if ext else "unknown"
+	}
