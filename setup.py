8b6fb77866d95e9b7c68fa2b947440e70b4778fb
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2016-07-22 18:07:32 -0400

    Release v1.2.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='fluffy-server',
-    version='1.2.0',
+    version='1.2.1',
     author='Chris Kuehl',
     author_email='ckuehl@ocf.berkeley.edu',
     packages=find_packages(),
