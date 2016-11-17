6e00cb9e1c015b2a9a7e8d951a5ee6acf0546507
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2016-11-17 17:33:02 -0500

    cli: add -r option to highlight

--- cli/fluffy_cli/main.py
+++ cli/fluffy_cli/main.py
@@ -5,6 +5,7 @@ It can be invoked directly, but is intended to be invoked by two aliases,
 "fput" and "fpb". fput uploads files, fpb pastes text.
 """
 import argparse
+import re
 import sys
 
 import requests
@@ -29,7 +30,7 @@ def upload(server, paths):
     print(bold(req.headers['Location']))
 
 
-def paste(server, path, language):
+def paste(server, path, language, highlight_regex):
     if path == '-':
         content = sys.stdin.read()
     else:
@@ -42,7 +43,32 @@ def paste(server, path, language):
         allow_redirects=False,
     )
     assert req.status_code in (301, 302), req.status_code
-    print(bold(req.headers['Location']))
+    location = req.headers['Location']
+
+    if highlight_regex:
+        matches = []
+        for i, line in enumerate(content.splitlines()):
+            if highlight_regex.search(line):
+                matches.append(i + 1)
+
+        # squash lines next to each-other
+        squashed = []
+        for match in matches:
+            if not squashed or squashed[-1][1] != match - 1:
+                squashed.append([match, match])
+            else:
+                squashed[-1][1] = match
+
+        if matches:
+            location += '#' + ','.join(
+                'L{}'.format(
+                    '{}-{}'.format(*match)
+                    if match[0] != match[1]
+                    else match[0]
+                ) for match in squashed
+            )
+
+    print(bold(location))
 
 
 def upload_main(argv=None):
@@ -65,9 +91,10 @@ def paste_main(argv=None):
     parser.add_argument('--server', default='https://fluffy.cc', type=str, help='server to upload to')
     parser.add_argument('--version', action='version', version='%(prog)s {}'.format(__version__))
     parser.add_argument('-l', '--language', type=str, default='autodetect', help='language for syntax highlighting')
+    parser.add_argument('-r', '--regex', type=re.compile, help='regex of lines to highlight')
     parser.add_argument('file', type=str, nargs='?', help='path to file to paste', default='-')
     args = parser.parse_args(argv)
-    return paste(args.server, args.file, args.language)
+    return paste(args.server, args.file, args.language, args.regex)
 
 
 if __name__ == '__main__':
