chriskuehl/fluffy

commit 62de1e4de16f96da91065214aa70a69956c5fc78

get data as plaintext for string pastes

fluffy/static/js/home.js
@@ -108,13 +108,12 @@ $(document).ready(function() {
 function pastefile(e) {
     var dt = (e.clipboardData || e.originalEvent.clipboardData);
     if (dt) {
-        if (dt.items.length === 1 && dt.items[0].type === 'text/plain') {
+        if (dt.items.length > 0 && dt.items[0].kind === 'string') {
             // user is trying to paste text
-            dt.items[0].getAsString(function(text) {
-                $('#text').text(text);
-                transitionToText(function() {
-                    $('#paste').click();
-                });
+            var text = dt.getData('text/plain');
+            $('#text').text(text);
+            transitionToText(function() {
+                $('#paste').click();
             });
         } else {
             // dunno what they're trying to paste, hopefully a file

commit 5e9852b63e7ff9298b761ac886c78fcbdcff4464

Fix pasting of images
This broke after 62de1e4de16f96da91065214aa70a69956c5fc78, at least on
Chrome, because often pasting an image has a "text/html" as the *first*
element.

This seems safer, since text should always have *only* string elements,
whereas a file paste might have some string elements (but obviously at
least one non-string element).

fluffy/static/js/home.js
@@ -108,7 +108,19 @@ $(document).ready(function() {
 function pastefile(e) {
     var dt = (e.clipboardData || e.originalEvent.clipboardData);
     if (dt) {
-        if (dt.items.length > 0 && dt.items[0].kind === 'string') {
+        // When copying an image from the web, often there's one item like
+        // "text/html" at the start, but then an image later on.
+        //
+        // By contrast, copying text sometimes results in several items, but
+        // they're all of kind "string".
+        var allStrings = true;
+        for (var i = 0; i < dt.items.length; i++) {
+            if (dt.items[i].kind !== 'string') {
+                allStrings = false;
+            }
+        }
+
+        if (dt.items.length > 0 && allStrings) {
             // user is trying to paste text
             var text = dt.getData('text/plain');
             $('#text').text(text);