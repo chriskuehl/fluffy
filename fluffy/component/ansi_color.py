3eaabcaa52957b69d3af57fc13c5a5a6d017a9ab
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2017-08-23 16:12:09 -0400

    Fix case where non-color ANSI codes gobble real text

--- fluffy/component/ansi_color.py
+++ fluffy/component/ansi_color.py
@@ -46,35 +46,51 @@ def token_from_lexer_state(bold, fg_color, bg_color):
         return getattr(Color, token_name)
 
 
-def callback(lexer, match):
-    codes = match.group(1).split(';')
+def _callback(lexer, match):
+    # http://ascii-table.com/ansi-escape-sequences.php
+    # "after_escape" contains everything after the escape sequence, up to the
+    # next escape sequence. We still need to separate the content from the end
+    # of the escape sequence.
+    after_escape = match.group(1)
 
-    for code in codes:
-        try:
-            code = int(code)
-        except ValueError:
-            # This shouldn't ever happen if we're given valid ANSI color codes,
-            # but people can pastebin junk, and we should tolerate that.
-            continue
-        else:
-            fg_color = _ansi_code_to_color.get(code - 30)
-            bg_color = _ansi_code_to_color.get(code - 40)
-            if fg_color:
-                lexer.fg_color = fg_color
-            elif bg_color:
-                lexer.bg_color = bg_color
-            elif code == 1:
-                lexer.bold = True
-            elif code == 22:
-                lexer.bold = False
-            elif code == 39:
-                lexer.fg_color = None
-            elif code == 49:
-                lexer.bg_color = None
-            elif code == 0:
-                lexer.reset_state()
+    # TODO: this doesn't handle the case where the values are non-numeric. This
+    # is rare but can happen for keyboard remapping, e.g. '\x1b[0;59;"A"p'
+    parsed = re.match(r'([0-9;=]*?)?([a-zA-Z])(.*)$', after_escape, re.DOTALL | re.MULTILINE)
+    if parsed is None:
+        # This shouldn't ever happen if we're given valid ANSI color codes,
+        # but people can pastebin junk, and we should tolerate that.
+        text = after_escape
+    else:
+        value, code, text = parsed.group(1), parsed.group(2), parsed.group(3)
 
-    yield match.start(), lexer.current_token(), match.group(2)
+        # We currently only handle "m" ("Set Graphics Mode")
+        if code == 'm':
+            values = value.split(';')
+            for value in values:
+                try:
+                    value = int(value)
+                except ValueError:
+                    # Shouldn't ever happen, but could with invalid ANSI codes.
+                    continue
+                else:
+                    fg_color = _ansi_code_to_color.get(value - 30)
+                    bg_color = _ansi_code_to_color.get(value - 40)
+                    if fg_color:
+                        lexer.fg_color = fg_color
+                    elif bg_color:
+                        lexer.bg_color = bg_color
+                    elif value == 1:
+                        lexer.bold = True
+                    elif value == 22:
+                        lexer.bold = False
+                    elif value == 39:
+                        lexer.fg_color = None
+                    elif value == 49:
+                        lexer.bg_color = None
+                    elif value == 0:
+                        lexer.reset_state()
+
+    yield match.start(), lexer.current_token(), text
 
 
 class AnsiColorLexer(pygments.lexer.RegexLexer):
@@ -97,6 +113,6 @@ class AnsiColorLexer(pygments.lexer.RegexLexer):
 
     tokens = {
         'root': [
-            (r'\x1b\[(.*?)m([^\x1b]*)', callback),
+            (r'\x1b\[([^\x1b]*)', _callback),
         ],
     }
