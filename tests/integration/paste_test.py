8a08a111d90bb690a865d90efb9e82ed45d622c1
Chris Kuehl <ckuehl@ckuehl.me>
2018-10-25 19:25:49 -0700

    Add a json option for paste

--- tests/integration/paste_test.py
+++ tests/integration/paste_test.py
@@ -1,3 +1,5 @@
+from unittest import mock
+
 import pytest
 import requests
 from pyquery import PyQuery as pq
@@ -20,3 +22,27 @@ def test_simple_paste(content, running_server):
         pq(req.content.decode('utf8')).find('input[name=text]').attr('value') ==
         content
     )
+
+
+def test_simple_paste_json(running_server):
+    req = requests.post(
+        running_server['home'] + '/paste?json',
+        data={
+            'text': 'hello world',
+            'language': 'autodetect',
+        },
+    )
+
+    assert req.status_code == 200
+    assert req.headers['Content-Type'] == 'application/json'
+    assert req.json() == {
+        'success': True,
+        'redirect': mock.ANY,
+        'uploaded_files': {
+            'paste': {
+                'raw': mock.ANY,
+                'paste': mock.ANY,
+                'metadata': mock.ANY,
+            },
+        },
+    }
